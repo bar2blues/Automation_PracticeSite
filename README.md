1b440578738edbaef0886dbf86bb5f1798e351c1
bar2blues <bar2blues@gmail.com>
2020-08-20 11:46:12 -0300

    add principal file and update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Automation_PracticeSite
+
+Proyecto de Automation desde cero con Selenium y Python, solo con fines de practica
---
+++ principal.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+import time
+
+class Items(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome('chromedriver.exe')
+        self.driver.get('http://automationpractice.com/index.php')
+
+    def test_view_item_page(self):
+        self.driver.find_element_by_id('search_query_top').send_keys('dress')
+        self.driver.find_element_by_name('submit_search').click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath('//*[@id="center_column"]/ul/li[1]/div/div[1]/div/a[1]/img').click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
